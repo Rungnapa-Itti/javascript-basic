003359cba0c0f5346f63b7aa55494d5681532cfc
rungnapa <rungnapa.itti@gmail.com>
2020-04-23 13:51:02 +0700

    update a

--- a.js
+++ a.js
@@ -1,4 +1,5 @@
 export const  a = "A"
+export const  b = "B"
 
 /*export default use one time per file
 export default function name(param){
@@ -6,4 +7,4 @@ export default function name(param){
 }
 */
 
-export default "Hello" //not hava variable 
+export default "Hello" // is anonymous -> not have variable , you have to create variable like line 224 in basic.js
